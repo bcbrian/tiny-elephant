d7f8d05501402d04f2ea8ff37dcb896ce0b5f708
Brian Bartholomew <me@bcbrian.com>
2021-08-26 17:59:57 -0700

    got the start of something great?

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
+import { useState } from "react";
 import "./styles.css";
 
+/**
+ * keep track of guesses
+ * flip cards over/hide values FLIP CARDS ANIMATION...
+ *        animation is nonblock for timing game?
+ * keep face up if matched/dont hide these
+ * style
+ * scale/levels easy medium hard
+ * rand values/shuffling use math.rand and splice?
+ * some ux options show the whole thing for a set amount of time
+ * track how quick you do it?
+ */
 export default function App() {
+  const NUMBER_OF_CARDS = 4;
+  const cards = [1, 1, 2, 2];
+
+  const [firstGuess, setFirstGuess] = useState(null);
+
+  function handleCardClick(event) {
+    const cardElem = event.target;
+    console.log(cardElem.name);
+    const cardIndex = cardElem.name;
+    const cardValue = cards[cardIndex];
+    console.log(cardValue);
+    if (firstGuess === null) {
+      setFirstGuess({ cardIndex, cardValue });
+      return;
+    }
+
+    if (firstGuess.cardIndex === cardIndex) {
+      console.log("you already clicked this :P");
+      return;
+    }
+
+    if (firstGuess.cardValue === cardValue) {
+      console.log("you found a match :P");
+      setFirstGuess(null);
+      return;
+    }
+    console.log("you did not find a match :P");
+    setFirstGuess(null);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="app">
+      <h1>Tiny Elephant</h1>
+      <div className="card-container">
+        {cards.map((card, i) => (
+          <button key={i} className="card" onClick={handleCardClick} name={i}>
+            {card}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
