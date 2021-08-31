90c1f697c4b5a19f2d31a77eae236f91393e1541
Brian Bartholomew <me@bcbrian.com>
2021-08-30 17:56:55 -0700

    made it better yo :P

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 /**
@@ -17,6 +17,8 @@ export default function App() {
   const cards = [1, 1, 2, 2];
 
   const [firstGuess, setFirstGuess] = useState(null);
+  const [secondGuess, setSecondGuess] = useState(null);
+  const [correctGuesses, setCorrectGuesses] = useState([]);
 
   function handleCardClick(event) {
     const cardElem = event.target;
@@ -24,8 +26,17 @@ export default function App() {
     const cardIndex = cardElem.name;
     const cardValue = cards[cardIndex];
     console.log(cardValue);
+    if (correctGuesses.includes(cardIndex)) {
+      console.log("you already know this :P");
+      return;
+    }
     if (firstGuess === null) {
       setFirstGuess({ cardIndex, cardValue });
+      return;
+    }
+
+    if (secondGuess === null && firstGuess.cardIndex !== cardIndex) {
+      setSecondGuess({ cardIndex, cardValue });
       return;
     }
 
@@ -34,14 +45,32 @@ export default function App() {
       return;
     }
 
-    if (firstGuess.cardValue === cardValue) {
-      console.log("you found a match :P");
-      setFirstGuess(null);
+    if (secondGuess.cardIndex === cardIndex) {
+      console.log("you already clicked this :P");
       return;
     }
-    console.log("you did not find a match :P");
-    setFirstGuess(null);
   }
+
+  useEffect(() => {
+    let handle = 0;
+    if (firstGuess !== null && secondGuess !== null) {
+      handle = setTimeout(() => {
+        if (firstGuess.cardValue === secondGuess.cardValue) {
+          console.log("you found a match :P");
+          setCorrectGuesses([
+            ...correctGuesses,
+            firstGuess.cardIndex,
+            secondGuess.cardIndex
+          ]);
+        } else {
+          console.log("you did not find a match :P");
+        }
+        setFirstGuess(null);
+        setSecondGuess(null);
+      }, 1000);
+    }
+    return () => clearTimeout(handle);
+  }, [firstGuess, secondGuess, setFirstGuess, setSecondGuess]);
 
   return (
     <div className="app">
@@ -49,7 +78,11 @@ export default function App() {
       <div className="card-container">
         {cards.map((card, i) => (
           <button key={i} className="card" onClick={handleCardClick} name={i}>
-            {card}
+            {i.toString() === firstGuess?.cardIndex ||
+            i.toString() === secondGuess?.cardIndex ||
+            correctGuesses.includes(i.toString())
+              ? card
+              : ""}
           </button>
         ))}
       </div>
